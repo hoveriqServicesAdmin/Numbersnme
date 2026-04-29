e825a1aeca393348641ae046aa8b1bb0b0213ca1
amitparab85 <69798246+amitparab85@users.noreply.github.com>
2026-04-30 04:32:53 +0530

    code on 30th Apr

--- _inspect_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect(r"C:\Users\Admin\PycharmProjects\Numbersnme\numbersnme.db")
-cur = conn.cursor()
-print('tables', cur.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name").fetchall())
-print('appointments schema', cur.execute("PRAGMA table_info(appointments)").fetchall())
-print('appointment indexes', cur.execute("SELECT name, sql FROM sqlite_master WHERE type='index' AND tbl_name='appointments'").fetchall())
-print('appointments count', cur.execute("SELECT COUNT(*) FROM appointments").fetchone()[0])
-conn.close()
